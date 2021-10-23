5c6e91a75b7500de0f68a057bf6684fb80161521
Patrick Matos <pmatos96@hotmail.com>
2021-10-23 18:02:04 -0300

    Voltando a porta 3333

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.PORT || 80, () => console.log('Em execução na porta 80'));
+app.listen(process.env.PORT || 3333, () => console.log('Em execução na porta 3333'));
 
