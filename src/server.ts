99ccdd2469b8d87a5d597b0fa0c8264d23343461
Patrick Matos <pmatos96@hotmail.com>
2021-10-23 18:05:52 -0300

    Teste without port

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => console.log('Em execução na porta 3333'));
+app.listen(3333, () => console.log('Em execução na porta 3333'));
 
